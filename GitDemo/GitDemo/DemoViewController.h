214aefe8287563e7d7d3872dbd9b1c0863d9dc21
NineCity <1171169229@qq.com>
2017-02-16 15:15:45 +0800

    增加一个项目
    增加一个测试项目，但是改变背景色的代码不做提交

---
+++ GitDemo/GitDemo/DemoViewController.h
@@ -0,0 +1,13 @@
+//
+//  DemoViewController.h
+//  GitDemo
+//
+//  Created by mac on 17/2/15.
+//  Copyright © 2017年 tc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoViewController : UIViewController
+
+@end
---
+++ GitDemo/GitDemo/TestViewController.m
@@ -0,0 +1,38 @@
+//
+//  TestViewController.m
+//  GitDemo
+//
+//  Created by mac on 17/2/15.
+//  Copyright © 2017年 tc. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
